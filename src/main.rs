defc5d37c151f6b4fddef64947daa5f51f66ffb7
Bipin Kalakheti <100770518+Bipin-Kalakheti@users.noreply.github.com>
2024-11-20 12:03:35 -0500

    Refactor HTTP response handling to streamline status code and content retrieval

--- src/main.rs
+++ src/main.rs
@@ -17,30 +17,17 @@ fn handle_connection(mut stream: TcpStream) {
     stream.read(&mut buffer).unwrap();
 
     let get = b"GET / HTTP/1.1\r\n";
-    if buffer.starts_with(get) {
-        send_index(stream);
+
+    let (status_line, filename) = if buffer.starts_with(get) {
+        ("HTTP/1.1 200 OK\r\n\r\n", "index.html")
     } else {
-        send_404(stream);
-    }
-}
-
-fn send_404(mut stream: TcpStream) {
-    let contents = fs::read_to_string("404.html").unwrap();
+        ("HTTP/1.1 404 NOT FOUND\r\n\r\n", "404.html")
+    };
+    let contents = fs::read_to_string(filename).unwrap();
 
     let response = format!(
-        "HTTP/1.1 404 NOT FOUND\r\nContent-Length: {}\r\n\r\n{}",
-        contents.len(),
-        contents
-    );
-    stream.write(response.as_bytes()).unwrap();
-    stream.flush().unwrap();
-}
-
-fn send_index(mut stream: TcpStream) {
-    let contents = fs::read_to_string("index.html").unwrap();
-
-    let response = format!(
-        "HTTP/1.1 200 OK\r\nContent-Length: {}\r\n\r\n{}",
+        "{}\r\nContent-Length: {}\r\n\r\n{}",
+        status_line,
         contents.len(),
         contents
     );
