ceb41b5e67fa03306cee8ebd166fa771d58be311
Bipin Kalakheti <100770518+Bipin-Kalakheti@users.noreply.github.com>
2024-11-22 08:24:18 -0500

    Refactor ThreadPool to use a Message enum for job handling and termination

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,7 @@ fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
     let pool = ThreadPool::new(4);
 
-    for stream in listener.incoming() {
+    for stream in listener.incoming().take(2) {
         let stream = stream.unwrap();
 
         pool.execute(|| {
@@ -28,21 +28,22 @@ fn handle_connection(mut stream: TcpStream) {
     let sleep = b"GET /sleep HTTP/1.1\r\n";
 
     let (status_line, filename) = if buffer.starts_with(get) {
-        ("HTTP/1.1 200 OK\r\n\r\n", "index.html")
+        ("HTTP/1.1 200 OK", "index.html")
     } else if buffer.starts_with(sleep) {
         thread::sleep(Duration::from_secs(5));
-        ("HTTP/1.1 200 OK\r\n\r\n", "index.html")
+        ("HTTP/1.1 200 OK", "index.html")
     } else {
-        ("HTTP/1.1 404 NOT FOUND\r\n\r\n", "404.html")
+        ("HTTP/1.1 404 NOT FOUND", "404.html")
     };
+
     let contents = fs::read_to_string(filename).unwrap();
+    let length = contents.len();
 
     let response = format!(
-        "{}\r\nContent-Length: {}\r\n\r\n{}",
-        status_line,
-        contents.len(),
-        contents
+        "{}\r\nContent-Length: {}\r\nContent-Type: text/html\r\n\r\n{}",
+        status_line, length, contents
     );
+
     stream.write(response.as_bytes()).unwrap();
     stream.flush().unwrap();
 }
